c2c6966f6bf0252ecf39bbfb676488f54aae8649
shangguanwei <coopershang125@gmail.com>
2020-09-15 22:02:29 +0000

    assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,31 @@
+// FUNCTION IMPLEMENTATION
+const assertArraysEqual = function(actual, expected) {
+  let goodEmoji = String.fromCodePoint(0x1F525);
+  let badEmoji = String.fromCodePoint(0x1F608);
+  if (eqArrays(actual,expected)) {
+    console.log(`${goodEmoji}${goodEmoji}${goodEmoji}Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${badEmoji}${badEmoji}${badEmoji}Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    let result;
+    for (let index in array1) {
+      if (array1[index] === array2[index]) {
+        result = true;
+      } else {
+        result = false;
+      }
+      if (!result) {
+        return result;
+      }
+    }
+    return result;
+  }
+};
+// TEST CODE
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
